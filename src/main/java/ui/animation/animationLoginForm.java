d852a1a176d064568a58d086c1ab2432dca618e4
maksy <maksymmykytuk@gmail.com>
2016-07-16 19:40:11 +0300

    16.07

--- src/main/java/ui/animation/animationLoginForm.java
+++ src/main/java/ui/animation/animationLoginForm.java
@@ -1,13 +1,11 @@
 package ui.animation;
 
-import javafx.animation.KeyFrame;
-import javafx.animation.KeyValue;
-import javafx.animation.Timeline;
+import javafx.animation.*;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.util.Duration;
-import ui.control.loginController;
-import ui.control.loginController.*;
+
+import java.security.Key;
 
 /**
  * Created by maksy on 12-Jul-16.
@@ -16,7 +14,7 @@ public class animationLoginForm {
 
     public static boolean Position;
 
-    public static void showPanel(Pane pane, Label label, Label cls) {
+    public static void showCreateUser(Pane pane, Label label, Label cls, Label hlp) {
         final Timeline slideOut = new Timeline();
         int y, x;
         String s;
@@ -30,16 +28,47 @@ public class animationLoginForm {
         } else {
             y = -407;
             x = -38;
-            Position = true;
             s = "Log in";
+            Position = true;
         }
         final KeyValue kv1 = new KeyValue(pane.translateYProperty(), y);
         final KeyFrame kf1 = new KeyFrame(Duration.millis(500), kv1);
-        final KeyValue kv2 = new KeyValue(label.textProperty(),s);
+        final KeyValue kv2 = new KeyValue(label.textProperty(), s);
         final KeyFrame kf2 = new KeyFrame(Duration.millis(500), kv2);
         final KeyValue kv3 = new KeyValue(cls.translateXProperty(), x);
         final KeyFrame kf3 = new KeyFrame(Duration.millis(10), kv3);
-        slideOut.getKeyFrames().addAll(kf1, kf2, kf3);
+        final KeyValue kv4 = new KeyValue(hlp.translateXProperty(), Math.abs(x));
+        final KeyFrame kf4 = new KeyFrame(Duration.millis(10), kv4);
+        slideOut.getKeyFrames().addAll(kf1, kf2, kf3, kf4);
+        slideOut.play();
+    }
+
+    public static void verification(Pane pane, Label check, Boolean aBoolean) {
+        verificationPanel(pane);
+        verificationCheck(check, aBoolean);
+    }
+
+    private static void verificationPanel(Pane pane) {
+        final Timeline slideOut = new Timeline();
+        pane.setOpacity(0);
+        final KeyValue kv1 = new KeyValue(pane.translateXProperty(), -372);
+        final KeyFrame kf1 = new KeyFrame(Duration.millis(.1), kv1);
+        final KeyValue kv2 = new KeyValue(pane.opacityProperty(), 1);
+        final KeyFrame kf2 = new KeyFrame(Duration.millis(100), kv2);
+
+        slideOut.getKeyFrames().addAll(kf1, kf2);
+        slideOut.play();
+    }
+
+    private static void verificationCheck(Label check, Boolean aBoolean) {
+        final Timeline slideOut = new Timeline();
+        if(aBoolean){check.setStyle("-fx-background-image: url('/ui/img/loginTrue.png')");}else{check.setStyle("-fx-background-image: url('/ui/img/loginFalse.png')");}
+        check.setOpacity(0);
+        final KeyValue kv1 = new KeyValue(check.opacityProperty(), 1);
+        final KeyFrame kf1 = new KeyFrame(Duration.millis(1000), kv1);
+        final KeyFrame kf2 = new KeyFrame(Duration.millis(4000));
+
+        slideOut.getKeyFrames().addAll(kf1, kf2);
         slideOut.play();
     }
 }
--- src/main/java/ui/control/loginController.java
+++ src/main/java/ui/control/loginController.java
@@ -1,13 +1,20 @@
 package ui.control;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.EventHandler;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 
 import java.net.URL;
-import java.util.ResourceBundle;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 ;import static ui.animation.animationLoginForm.*;
 
@@ -20,12 +27,123 @@ public class loginController implements Initializable {
     public Pane login_panel;
     public Label creat_user;
     public Label cls_button_d;
+    public Label help_button;
+    public Label help_button_d;
+    public Label checkLogin;
+    public Label checkPassword;
+    public Label checkCPassword;
+    public Label checkEmail;
+    public Button createUser;
+    public TextField fieldLogin;
+    public TextField fieldCPassword;
+    public TextField fieldMail;
+    public TextField fieldPassword;
+    public PasswordField password;
+    public TextField username;
+    public Pane verification;
+    public Label check;
+    public Button login_button;
 
     public void initialize(URL url, ResourceBundle rb) {
+        exit(cls_button);
+        exit(cls_button_d);
+
         creat_user.addEventFilter(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
-                showPanel(login_panel, creat_user, cls_button_d);
+                if (event.getButton() == MouseButton.PRIMARY) {
+                    showCreateUser(login_panel, creat_user, cls_button_d, help_button_d);
+                    fieldPassword.setText("");
+                    checkPassword.setStyle("");
+                    fieldCPassword.setText("");
+                    checkCPassword.setStyle("");
+                    fieldMail.setText("");
+                    checkEmail.setStyle("");
+                    fieldLogin.setText("");
+                    checkLogin.setStyle("");
+                }
+            }
+        });
+
+        fieldCPassword.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                if (fieldCPassword.getText().trim().equals(fieldPassword.getText().trim())) {
+                    checkCPassword.setStyle("-fx-background-image: url('/ui/img/checkTrue.png')");
+                } else {
+                    checkCPassword.setStyle("-fx-background-image: url('/ui/img/checkFalse.png')");
+                }
+            }
+        });
+
+        fieldPassword.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                if (fieldCPassword.getText().trim().equals(fieldPassword.getText().trim())) {
+                    checkCPassword.setStyle("-fx-background-image: url('/ui/img/checkTrue.png')");
+                } else {
+                    checkCPassword.setStyle("-fx-background-image: url('/ui/img/checkFalse.png')");
+                }
+                checkPassword.setStyle("-fx-background-image: url('/ui/img/checkTrue.png')");
+            }
+        });
+
+        creatUser();
+
+        login();
+    }
+
+    public void login() {
+        login_button.addEventFilter(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                if(event.getButton() == MouseButton.PRIMARY){
+                    if (!(password.getText().trim().isEmpty() && username.getText().trim().isEmpty())) {
+                        verification(verification, check, true);
+                    }
+                }
+            }
+        });
+    }
+
+    public void creatUser() {
+        createUser.addEventFilter(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                if (mouseEvent.getButton() == MouseButton.PRIMARY) {
+                    if (fieldLogin.getText().trim().isEmpty()) {
+                        checkLogin.setStyle("-fx-background-image: url('/ui/img/checkFalse.png')");
+                    } else {
+                        checkLogin.setStyle("-fx-background-image: url('/ui/img/checkTrue.png')");
+                    }
+
+                    if (fieldPassword.getText().trim().isEmpty()) {
+                        checkPassword.setStyle("-fx-background-image: url('/ui/img/checkFalse.png')");
+                        checkCPassword.setStyle("-fx-background-image: url('/ui/img/checkFalse.png')");
+                    } else {
+                        checkPassword.setStyle("-fx-background-image: url('/ui/img/checkTrue.png')");
+                    }
+
+                    if (fieldMail.getText().trim().isEmpty()) {
+                        checkEmail.setStyle("-fx-background-image: url('/ui/img/checkFalse.png')");
+                    } else {
+                        checkEmail.setStyle("-fx-background-image: url('/ui/img/checkTrue.png')");
+                    }
+
+                    if (!(fieldPassword.getText().trim().isEmpty() && fieldLogin.getText().trim().isEmpty()
+                            && fieldCPassword.getText().trim().isEmpty() && fieldMail.getText().trim().isEmpty())) {
+                        //create user
+                    }
+                }
+            }
+        });
+    }
+
+    public void exit(Label label) {
+        label.addEventFilter(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                System.exit(0);
             }
         });
     }
